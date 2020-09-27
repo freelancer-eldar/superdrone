ffa6a65e3efd0375065c465a72337ccec31c310d
freelancer-eldar <69983229+freelancer-eldar@users.noreply.github.com>
2020-09-27 09:39:59 +0300

    v3

--- source/js/main.js
+++ source/js/main.js
@@ -1,16 +1,32 @@
 $(document).ready(function(){
-
 	function parallax(c,l){
 		$(c).css("top",l+"px");
 	};
 	parallax(".page__bgi",Math.round(window.pageYOffset/7));
 	window.addEventListener("scroll",function (event){
 		let py = window.pageYOffset;
-		parallax(".page__bgi",Math.round(py/7));
+		parallax(".page__bgi",Math.round(py/5));
 	});
 
 	$(".burger").on("click",function(){
-		$(this).toggleClass("active");
+		$(".burger,.nav").toggleClass("active");
 		$("body").toggleClass("lock");
+	});
+	
+	var addLogin = false;
+	function addLinks(ww){
+		if (ww<=600 && !addLogin){
+			addLogin = true;
+			var links = $(".header__column:last-child").html();
+			console.log(addLogin);
+
+			document.querySelector(".nav").innerHTML+="<div class=\"link-row df\">"+links+"</div>";
+		};
+	};
+	addLinks(window.innerWidth);
+	window.addEventListener("resize",function(){
+		let ww = window.innerWidth;
+
+		addLinks(ww);
 	});
 });
